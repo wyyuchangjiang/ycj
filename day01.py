31d6020b011b0654ba36fe61e2f2de6e8795e8d1
root <root@localhost.localdomain>
2018-09-25 03:53:34 -0400

    test

---
+++ day01.py
@@ -0,0 +1,13 @@
+'''a = raw_input(">>") # return str
+b = raw_input(">>") # return str
+int_a = int(a) #int :: change to int type
+int_b = int(b) #int :: change to int type
+s = int_a * int_b
+print(s)'''
+'''a = raw_input(">>")
+b = a/60
+c = a%60
+print(b,c)'''
+a = (3 + 4)/5  + 9*(4/1 + (9+1)/1)
+print(a)
+
